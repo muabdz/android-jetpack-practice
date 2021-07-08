200183d0e211f3b222afa188072806f9b0614467
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2021-07-08 21:26:48 +0700

    implement live data on life cycle owners and observe them

--- app/src/main/java/com/muabdz/academy/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/muabdz/academy/ui/bookmark/BookmarkFragment.kt
@@ -28,9 +28,14 @@ class BookmarkFragment : Fragment(), BookmarkFragmentCallBack {
         if (activity != null) {
             val factory = ViewModelFactory.getInstance(requireActivity())
             val viewModel = ViewModelProvider(this, factory)[BookmarkViewModel::class.java]
-            val courses = viewModel.getBookMarks()
             val adapter = BookmarkAdapter(this)
-            adapter.setCourses(courses)
+
+            fragmentBookmarkBinding.progressBar.visibility = View.VISIBLE
+            viewModel.getBookMarks().observe(viewLifecycleOwner, {courses ->
+                fragmentBookmarkBinding.progressBar.visibility = View.GONE
+                adapter.setCourses(courses)
+                adapter.notifyDataSetChanged()
+            })
 
             with(fragmentBookmarkBinding.rvBookmark) {
                 layoutManager = LinearLayoutManager(context)
