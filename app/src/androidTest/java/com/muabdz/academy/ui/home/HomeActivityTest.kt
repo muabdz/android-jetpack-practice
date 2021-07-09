e57d1c5acda977fe553f9ee665abb3acca3cd4d5
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2021-07-09 07:36:14 +0700

    add idling resource implementation on instrumental testing class
    the implementation on increment and decrement removed to avoid memory leak

--- app/src/androidTest/java/com/muabdz/academy/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/muabdz/academy/ui/home/HomeActivityTest.kt
@@ -1,7 +1,9 @@
 package com.muabdz.academy.ui.home
 
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -9,6 +11,9 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.muabdz.academy.R
 import com.muabdz.academy.utils.DataDummy
+import com.muabdz.academy.utils.EspressoIdlingResource
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -18,18 +23,26 @@ class HomeActivityTest {
     @get:Rule
     var activityRule = ActivityScenarioRule(HomeActivity::class.java)
 
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(HomeActivity::class.java)
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.idlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.idlingResource)
+    }
+
     @Test
     fun loadCourses() {
-        delayTwoSeconds()
         onView(withId(R.id.rv_academy)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
     }
 
     @Test
     fun loadDetailCourse() {
-        delayTwoSeconds()
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        delayTwoSeconds()
         onView(withId(R.id.tv_course_title)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_course_title)).check(matches(withText(dummyCourse[0].title)))
         onView(withId(R.id.tv_date)).check(matches(isDisplayed()))
@@ -38,39 +51,23 @@ class HomeActivityTest {
 
     @Test
     fun loadModule() {
-        delayTwoSeconds()
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        delayTwoSeconds()
         onView(withId(R.id.btn_start)).perform(click())
-        delayTwoSeconds()
         onView(withId(R.id.rv_module)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadDetailModule() {
-        delayTwoSeconds()
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        delayTwoSeconds()
         onView(withId(R.id.btn_start)).perform(click())
-        delayTwoSeconds()
         onView(withId(R.id.rv_module)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        delayTwoSeconds()
         onView(withId(R.id.web_view)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadBookmarks() {
         onView(withText("Bookmark")).perform(click())
-        delayTwoSeconds()
         onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_bookmark)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
-    }
-
-    private fun delayTwoSeconds() {
-        try {
-            Thread.sleep(2000)
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
-        }
     }
 }
