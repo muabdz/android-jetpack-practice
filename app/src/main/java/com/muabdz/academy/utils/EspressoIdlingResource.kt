38194d59d20edc7aa38bf203e0bfd1b458751ce0
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2021-07-09 07:35:12 +0700

    add idling resource global class

---
+++ app/src/main/java/com/muabdz/academy/utils/EspressoIdlingResource.kt
@@ -0,0 +1,17 @@
+package com.muabdz.academy.utils
+
+import androidx.test.espresso.idling.CountingIdlingResource
+
+object EspressoIdlingResource {
+
+    private const val RESOURCE = "Global"
+    val idlingResource = CountingIdlingResource(RESOURCE)
+
+    fun increment() {
+        idlingResource.increment()
+    }
+
+    fun decrement() {
+        idlingResource.decrement()
+    }
+}
